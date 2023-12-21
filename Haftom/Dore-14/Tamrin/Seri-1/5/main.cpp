c013b9c9468df63bca57e21202727c795873cc60
Pishgard <ali.pishgard79@gmail.com>
2023-12-21 14:58:04 +0330

    Add Seri 1 Haftom

---
+++ Haftom/Dore-14/Tamrin/Seri-1/5/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if (a % 3 == 0)
+    {
+        cout << "Yes";
+    }
+    if (a % 3 != 0)
+    {
+        cout << "No";
+    }
+}
